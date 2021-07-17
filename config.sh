c1a051835ccfce5befb57262d101763e1abf699a
Mr Four <mrfourit1994@gmail.com>
2021-07-17 23:36:00 +0700

    Update limit projext

--- config.sh
+++ config.sh
@@ -460,13 +460,15 @@ limit_project() {
 show_yes_no_question() {
 				read -p "Ban co chac chan muon tiep tuc (Y|y|N|n): " yes_no
     
-    if [ yes_no != "Y" ] && [ yes_no != "y" ]
+    if [ [ yes_no != "Y" ] && [ yes_no != "y" ] ]
 						then
 										exit 1
 				fi
 }
                     
-show_question_increase_size_project() {}
+show_question_increase_size_project() {
+    echo "a"               
+}
 
 add_domain() {
     ALIAS_DOMAIN="";
